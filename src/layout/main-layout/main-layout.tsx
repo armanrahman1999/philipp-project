bd2ee0ee025f5f582be86933de70e6549456222b
armanrahman1999 <arman.rahman@selisegroup.com>
2026-01-13 15:15:44 +0600

    fix: layout fixed

--- src/layout/main-layout/main-layout.tsx
+++ src/layout/main-layout/main-layout.tsx
@@ -1,27 +1,17 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from '@/components/main/header';
 import Footer from '@/components/main/footer';
 
 export const MainLayout = () => {
-  const { pathname } = useLocation();
-  const segments = pathname?.split('/').filter(Boolean);
-  const firstSegment = segments?.[0] ?? undefined;
-  const isEmailRoute = firstSegment === 'mail';
-  const isChatRoute = firstSegment === 'chat';
-
   return (
     <div className="flex w-full min-h-screen relative">
       {/* <div className="absolute left-0 top-0 h-full">
         <AppSidebar />
       </div> */}
 
-      <div
-        className={`flex flex-col w-full h-full transition-[margin-left] duration-300 ease-in-out`}
-      >
+      <div className="flex flex-col w-full min-h-screen transition-[margin-left] duration-300 ease-in-out">
         <Header />
-        <div
-          className={`flex h-full bg-surface ${!isEmailRoute && !isChatRoute && 'p-4 sm:p-6 md:p-8'} `}
-        >
+        <div className="flex-1 flex flex-col bg-surface">
           <Outlet />
         </div>
         <Footer />
