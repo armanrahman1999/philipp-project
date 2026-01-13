0cc08ddd29e4f7feef448ad725f9c984571e73e4
armanrahman1999 <arman.rahman@selisegroup.com>
2026-01-13 17:53:41 +0600

    fix: basic changes made

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,11 +1,9 @@
-import { LanguageSelector } from '@/components/core';
-
 const Header: React.FC = () => {
   return (
     <header className="sticky top-0 z-10 flex items-center justify-between bg-white px-5 py-4 shadow-sm md:px-5">
       <div className="logo">
         <img
-          src="logo/Selise Logo G.svg"
+          src="/Selise Logo G.svg"
           alt="SELISE Digital Platforms"
           className="h-7 w-auto md:h-7"
         />
@@ -17,7 +15,6 @@ const Header: React.FC = () => {
         >
           Zurück zur Startseite
         </a>
-        <LanguageSelector />
       </div>
     </header>
   );
